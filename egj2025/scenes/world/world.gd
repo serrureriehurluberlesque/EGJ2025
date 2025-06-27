af8145f4b57ca605f47b6d7eb928b24b937f0bca
Elsa Weber <elsa.weber0@gmail.com>
2025-06-28 00:40:13 +0200

    Plus de checks pour planter les plantes

--- egj2025/scenes/world/world.gd
+++ egj2025/scenes/world/world.gd
@@ -2,12 +2,25 @@ extends TileMapLayer
 
 var plant_ = preload("res://scenes/plant/plant.tscn")
 
+const EARTH_TILES_ATLAS_COORDS = [
+	Vector2i(0,1),
+	Vector2i(1,1),
+	Vector2i(0,2),
+	Vector2i(1,2),
+]
+
+var plant_coords = []
+
 func _input(event):
 	if event.is_action_pressed("activate"):
 		var map_coords = self.local_to_map(event.position)
-		self.set_cell(map_coords, 2, Vector2i(2,1))
 		
-		# TODO check si la cell est ok (terre + pas encore de plante à cet endroit)
-		var new_plant = plant_.instantiate()
-		new_plant.position = self.map_to_local(map_coords)
-		add_child(new_plant)
+		print(self.get_cell_atlas_coords(map_coords))
+		
+		if self.get_cell_atlas_coords(map_coords) in EARTH_TILES_ATLAS_COORDS and map_coords not in plant_coords:
+			self.set_cell(map_coords, 2, Vector2i(2,1))
+			var new_plant = plant_.instantiate()
+			new_plant.position = self.map_to_local(map_coords)
+			new_plant.z_index = map_coords[1]
+			add_child(new_plant)
+			plant_coords.append(map_coords)
