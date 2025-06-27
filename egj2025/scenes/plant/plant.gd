c353c03d1efcc7875a719c7293ca05a620fa7d5c
Hugo Dupraz <hugo.dupraz@gmail.com>
2025-06-27 23:19:14 +0200

    add esquisse of function for plants

--- egj2025/scenes/plant/plant.gd
+++ egj2025/scenes/plant/plant.gd
@@ -1,6 +1,8 @@
 class_name Plant
 extends Node2D
 
+@export var is_legal := false
+@export var value := 100.0
 @export var max_growth := 100.0
 
 var growth := 0.0
@@ -13,3 +15,14 @@ func _process(delta: float) -> void:
 func grow(speed: float) -> void:
 	growth = min(max_growth, growth + speed)
 	$Top.position = Vector2(0, -growth)
+
+
+func saw_illegal() -> bool:
+	return not is_legal and growth > 20.0
+
+
+func get_cut() -> float:
+	if not is_legal and growth == max_growth:
+		return value
+	else:
+		return 0.0
