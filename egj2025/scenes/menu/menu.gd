55cba733364f2ed5870a5f1b4f8b0de6b354e5ad
Elsa Weber <elsa.weber0@gmail.com>
2025-06-28 22:32:53 +0200

    un peu mieux

--- egj2025/scenes/menu/menu.gd
+++ egj2025/scenes/menu/menu.gd
@@ -7,20 +7,20 @@ Vous êtes un pauvre cultivateur de fleur, et deux choix s'offrent à vous: cult
 Arriverez-vous à faire fortune en étant plus malin que les autorités ?\n
 [u]Outils[/u]
 [ul]
-[b]Sac graines bleues[/b]: plante une graine de fleur normale
-[b]Sac graines rouges[/b]: plante une graine de fleur Ouïd [i]interdite[/i]
-[b]Cisailles[/b]: récolte une fleur si elle est à majorité; coupe la plante sans résultat sinon.
-[b]Arrosoir[/b]: permet d'accélérer la pousse des fleurs.
+[b]Sac graines bleues [lb]1[rb][/b]: plante une graine de fleur normale
+[b]Sac graines rouges [lb]2[rb][/b]: plante une graine de fleur Ouïd [i]interdite[/i]
+[b]Cisailles [lb]3[rb][/b]: récolte une fleur si elle est à majorité; coupe la plante sans résultat sinon.
+[b]Arrosoir [lb]4[rb][/b]: permet d'accélérer la pousse des fleurs.
 [/ul]\n
 [u]Prix[/u]
 [center][table=3]
 [cell][/cell][cell]Coût ($)[/cell][cell]Revenu ($)[/cell]
-[cell]fleur bleue[/cell][cell]%d[/cell][cell]%d[/cell]
-[cell]fleur rouge[/cell][cell]%d[/cell][cell]%d[/cell]
+[cell] [img]res://scenes/Bulle/assets/bleu.png[/img] [/cell][cell]%d[/cell][cell]%d[/cell]
+[cell] [img]res://scenes/Bulle/assets/rouge.png[/img] [/cell][cell]%d[/cell][cell]%d[/cell]
 [/table][/center]
 "
 
-const COST_REVENUE_LIST = [1,1,1,2]
+const COST_REVENUE_LIST = [5, 5, 5, 25]
 
 func _ready() -> void:
 	$Background/Explanation.text = explanation % COST_REVENUE_LIST
