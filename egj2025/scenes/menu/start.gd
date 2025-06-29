d6423de20ef8a93ec12c4e306514d0f9be7086a8
Hugo Dupraz <hugo.dupraz@gmail.com>
2025-06-29 14:40:37 +0200

    add small timing at the start

--- egj2025/scenes/menu/start.gd
+++ egj2025/scenes/menu/start.gd
@@ -1,4 +1,12 @@
 extends TextureRect
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world/world.tscn")
+var t = 0
+
+func _process(delta: float) -> void:
+	t += delta
+
+func _unhandled_input(event: InputEvent) -> void:
+	if t > 0.5:
+		if event.is_pressed():
+			get_tree().change_scene_to_file("res://scenes/world/world.tscn")
+ 
